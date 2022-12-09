99f7047eac5f3bf6f25cbee17b3db9a082d72d3f
Jeferson Apaza <jefersonapaza@gmail.com>
2022-12-09 11:03:17 -0500

    Update Controllers

--- src/main/java/com/nttdata/bootcamp/mscredit/service/DatabaseSequenceService.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.bootcamp.mscredit.service;
-
-import reactor.core.publisher.Mono;
-
-public interface DatabaseSequenceService {
-
-    Mono<Long> generateSequence(String seqName);
-
-}
